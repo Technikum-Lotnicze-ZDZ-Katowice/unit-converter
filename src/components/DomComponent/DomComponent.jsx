81e7552cab64a304a7273facf9dc73a07fe803a6
Dominiczek <nomail@mail.org>
2025-11-19 08:58:36 +0100

    Add bounds

--- src/components/DomComponent/DomComponent.jsx
+++ src/components/DomComponent/DomComponent.jsx
@@ -3,8 +3,10 @@ import React, { useState } from 'react';
 const converter = (value) => {
   const [fahrenheit, ustawFahrenheit] = useState('');
 
+  const color = value.value > 100 ? 'red' : 'black';
+
   return (
-      <p>
+      <p style={{ color }}>
         {`${value.value}°F = ${((value.value - 32) * 5) / 9}°C`}
       </p>
   );
